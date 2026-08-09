584b1dada7b1474931296948dc11d91150701363
Leonardo <leonardo.mejiam@hotmail.com>
2026-08-08 21:46:23 -0700

    Fix Dashboard import

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css'
 import Login from './views/Login'
+import Dashboard from './views/Dashboard'
 
 
 
