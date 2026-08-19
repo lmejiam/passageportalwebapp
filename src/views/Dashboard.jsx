59c7b6d0122d77860d3c21f11bec70aa36ab12f6
Leonardo <leonardo.mejiam@hotmail.com>
2026-08-19 16:51:16 -0700

    change table header

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -48,14 +48,14 @@ const Dashboard = ()=>{
 
                         <table>
                             <thead className="flex justify-between items-center text-xl font-bold font-mono border-2 border-whooshhgreen">
-                                <th>TAG NUMBER</th>
-                                <th>PRIORITY</th>
-                                <th>CIRCUMFERENCE</th>
-                                <th>WEIGHT</th>
-                                <th>GATE</th>
-                                <th>OUTPUTLANE</th>
-                                <th>SORT INFO</th>
-                                <th>TIMESTAMP</th>
+                                <th className="p-5">TAG NUMBER</th>
+                                <th className="p-5">PRIORITY</th>
+                                <th className="p-5">CIRCUMFERENCE</th>
+                                <th className="p-5">WEIGHT</th>
+                                <th className="p-5">GATE</th>
+                                <th className="p-5">OUTPUTLANE</th>
+                                <th className="p-5">SORT INFO</th>
+                                <th className="p-5">TIMESTAMP</th>
                             </thead>
                             <tbody>
                                 {fish.map(f =>(
